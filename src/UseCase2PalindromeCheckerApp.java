a438c17c0b711b6f26add6f4aa71206f5e0f050d
ciyachitranshi81 <cs2945@srmist.edu.in>
2026-02-20 10:03:14 +0530

    UC2: Hardcoded palindrome check implemented

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args){
+        String word = "madam";
+        String reversed = "";
+        for(int i = word.length() -1; i >= 0; i-- ){
+            reversed = reversed + word.charAt(i);
+        }
+        System.out.println("Input text :" +word);
+        if(word.equals(reversed))
+            System.out.println("Is it a Palindrome?: True");
+        else
+            System.out.println("Is it a Palindrome?: False");
+    }
+}
